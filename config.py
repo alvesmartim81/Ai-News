ed3d394b22010302bf2477940d2429e2be095d2e
AlvesMartim81 <alvesmartim81@gmail.com>
2025-10-24 10:45:56 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+from agents import NyTimes, Guardian
+
+api_nytimes = "minha api do nytimes"
+api_guardian = "minha api do guardian"
+api_gemini = "minha api do gemini"
+senha_email = "minha senha"
+email = "meu email"
+
+agents = {
+    "nytimes": (NyTimes, api_nytimes),
+        "guardian": (Guardian, api_guardian)
+}
